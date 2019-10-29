ce45511114e85a48fa054262d869a69ee334039b
Darcy Atlas <foreverdiffrent@live.com>
2019-10-29 03:44:35 -0700

    need help with getting site/server working

--- config/orm.js
+++ config/orm.js
@@ -6,13 +6,13 @@ var connection = require("../config/connection.js");
     //updateOne()
 
 var orm = {
-    selectAll: function(cb) {
+    selectAll: function(whatToSelect, cb) {
         var queryString = "SELECT * FROM burgers";
         connection.query(queryString, [whatToSelect], function(err, result) {
             if (err) throw err;
             console.log(result);
+            cb(result);
         });
-        cb(result);
     },
     insertOne: function(burger_name, isDevoured, cb) {
         var queryString = "INSERT INTO burgers (burger_name, devoured) ";
@@ -20,16 +20,16 @@ var orm = {
         connection.query(queryString, [burger_name, isDevoured], function(err, result) {
             if (err) throw err;
             console.log(result);
+            cb(result);
         });
-        cb(result);
     },
     updateOne: function(whatToSelect, valOfCol, cb) {
         var queryString = "UPDATE burgers SET devoured = true WHERE burger_name = ?";
         connection.query(queryString, [whatToSelect, valOfCol], function(err, result) {
             if (err) throw err;
             console.log(result);
+            cb(result);
         });
-        cb(result);
     }
 }
 
